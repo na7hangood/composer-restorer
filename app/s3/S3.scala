3a966fbc1b550a4730b375e8dafebe48e3d63d36
hmgibson23 <hugo.gibson@yahoo.co.uk>
2014-11-18 10:57:56 +0000

    File for the S3 interaction

---
+++ app/s3/S3.scala
@@ -0,0 +1,13 @@
+
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.PutObjectResult
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.Region
+import java.io.ByteArrayInputStream
+import scala.collection.JavaConverters._
+
+object S3 {
+  def getSnapshot(id: String, timestamp: String) = {
+  }
+}
