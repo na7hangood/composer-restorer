ee8557818dad3616b74c4732e402558fd89ef923
Robin Edman <robin.edman@guardian.co.uk>
2014-12-03 15:10:09 +0000

    UI for demo

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,25 +5,30 @@ var request = require('superagent');
 var apiUrl = 'https://composer.local.dev-gutools.co.uk/api';
 var apiContentUrl = apiUrl + '/content/54747d607d84173b978e38ec';
 
+function restore(versionId, isLive) {
+    //var archivedVersionPath = window.location.href + '?isLive=' + isLive + '&versionId=' + versionId;
+    request.get(apiContentUrl)
+        .withCredentials()
+        .set('Content-Type', 'text/plain')
+        .send("some text")
+        .end(function(error, response) {
+            console.log('Response ok:', response.ok);
+            console.log('Response text:', response.text);
+            console.log('Error? :', error);
+
+            request.post(apiContentUrl)
+                .withCredentials()
+                .set('Content-Type', 'text/plain')
+                .send("some text")
+                .end(function(error, response) {
+                    console.log('Response ok:', response.ok);
+                    console.log('Response text:', response.text);
+                    console.log('Error? :', error);
+                });
+        });
+}
+
 // It ain't pretty exposing it on `window`, but let's keep it simple for now.
 // Separating things into a single page app and an API would enable an equally
 // simple but nice solution.
-window.onRestoreBtnClick = function (versionId, isLive) {
-    var archivedVersionPath = window.location.href + '?isLive=' + isLive + '&versionId=' + versionId;
-    console.log(archivedVersionPath)
-    request.get(archivedVersionPath, function(response) {
-        console.log('Response ok:', response.ok);
-        console.log('Response text:', response.text);
-
-        var archivedVersion = response.text;
-        request.post(apiContentUrl)
-               .set('Content-Type', 'text/plain')
-               .send(archivedVersion)
-               .end(function(error, response) {
-                console.log('Response ok:', response.ok);
-                console.log('Response text:', response.text);
-                console.log('Error? :', error);
-        });
-    });
-
-};
+window.onRestoreBtnClick = restore;
