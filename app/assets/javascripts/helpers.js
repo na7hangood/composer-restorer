44ba0cb5fdbf9468a85e488eb571afda8e89524f
Robin Edman <robin.edman@guardian.co.uk>
2015-01-05 17:19:47 +0000

    Get env from URL. Also moved this to another file

---
+++ app/assets/javascripts/helpers.js
@@ -0,0 +1,30 @@
+// Get the environment from the URL.
+//
+// Assumes the URL looks something like:
+// 'https://composer-restorer.local.dev-gutools.co.uk/' (test environment) or
+// 'https://composer-restorer.dev-gutools.co.uk/' (production environment).
+function getEnvironment() {
+    var env = window.location.origin.split('.')[1];
+    return env === 'gutools' ? 'production' : env;
+}
+
+// Get the URL to Composer within the same environment.
+function getComposerUrl() {
+    var env = getEnvironment();
+    return env === 'production' ? 'https://composer.dev-gutools.co.uk'
+                                : 'https://composer.{{env}}.dev-gutools.co.uk'.replace('{{env}}', env);
+}
+
+// Get the URL to the Composer api within the same environment.
+function getApiUrl() {
+    return getComposerUrl() + '/api';
+}
+
+function restoreContentUrlFor(contentId) {
+    return getApiUrl() + '/restorer/content/:contentId'.replace(':contentId', contentId);
+}
+
+exports.getEnvironment = getEnvironment;
+exports.getComposerUrl = getComposerUrl;
+exports.getApiUrl = getApiUrl;
+exports.restoreContentUrlFor = restoreContentUrlFor;
